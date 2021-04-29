5467f7ea3a81d326ac2791a2b28016a5df340f4a
TB8466 <78532614+TB8466@users.noreply.github.com>
2021-04-29 11:38:31 +0200

    Add files via upload

---
+++ src/main/java/eksamensprojekt2semester/EksamensProjekt2SemesterApplication.java
@@ -0,0 +1,13 @@
+package eksamensprojekt2semester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EksamensProjekt2SemesterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EksamensProjekt2SemesterApplication.class, args);
+    }
+
+}
