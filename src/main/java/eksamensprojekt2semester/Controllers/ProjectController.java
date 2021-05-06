f35dd2cd668266ba710c94ad8bc8d9ed82957b06
Elkjaer97 <Elkjaer1997@gmail.com>
2021-05-06 15:12:55 +0200

    første udkast DataBase

---
+++ src/main/java/eksamensprojekt2semester/Controllers/ProjectController.java
@@ -0,0 +1,8 @@
+package eksamensprojekt2semester.Controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ProjectController {
+
+}
---
+++ src/main/java/eksamensprojekt2semester/DatabaseAccessLayer/ProjectManager.java
@@ -0,0 +1,22 @@
+package eksamensprojekt2semester.DatabaseAccessLayer;
+
+import eksamensprojekt2semester.Model.Project;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ProjectManager {
+
+    public void createProject(Project project) throws SQLException {
+        Connection connection = DBManager.getConnection();
+        String query = "INSERT INTO PROJECTS (PROJECT_NAME, PROJECT_DESC) VALUES(?,?)"; //prepare query
+        PreparedStatement preparedStatement = connection.prepareStatement(query); // tager imod query
+
+        preparedStatement.setString(1, project.getName());
+        preparedStatement.setString(2, project.getDescription());
+
+        preparedStatement.executeUpdate();
+
+    }
+}
